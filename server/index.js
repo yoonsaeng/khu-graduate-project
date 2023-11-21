c726c6c7dfbd5f09d3307280f69e7e5d7f42ebc9
yoonsaeng <truestar1997@gmail.com>
2023-11-21 19:32:20 +0900

    Express 서버 초기 세팅

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 
 function handleListening() {
   console.log(`Listening on: http://localhost:${PORT}`);
